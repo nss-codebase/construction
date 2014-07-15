a2e04b2c72e241cec94bca14f5c41ff54c73ee23
Chyld Medford <chyld.medford@gmail.com>
2014-07-15 15:57:27 -0500

    done

---
+++ app/house.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function House(name, material, year){
+  this.name = name;
+  this.material = material;
+  this.year = parseInt(year);
+  this.rooms = [];
+}
+
+House.prototype.area = function(){
+  var sum = 0;
+
+  for(var i = 0; i < this.rooms.length; i++){
+    sum += this.rooms[i].area();
+  }
+
+  return sum;
+};
+
+House.prototype.cost = function(){
+  var sum = 0;
+
+  for(var i = 0; i < this.rooms.length; i++){
+    sum += this.rooms[i].cost();
+  }
+
+  return sum;
+};
+
+module.exports = House;
+
--- app/index.js
+++ app/index.js
@@ -1,63 +1,23 @@
-/* jshint -W069 */
-/* global prompt:true */
+var Room = require('./room');
+var House = require('./house');
 
-'use strict';
+var r1 = new Room('living',  10, 12,  9, 'carpet', 'aqua');
+var r2 = new Room('dining',   8, 10, 10, 'tile',   'green');
+var r3 = new Room('bedroom',  9, 11, 12, 'wood',   'orange');
 
-// creating a object manually
-//
+console.log(r1, r2, r3);
 
-var dogs = [];
+var h1 = new House('M-4', 'brick', 1993);
+var h2 = new House('M-5', 'stone', 1785);
+var h3 = new House('M-6', 'straw', 1832);
 
-var d1 = {};
-d1.name = 'fido';
-d1.age = 3;
+h1.rooms.push(r1, r2, r3);
+console.log(h1, h2, h3);
 
-var d2 = {name:'spot', age:5};
-
-var d3 = {};
-d3['name'] = 'lassy';
-d3['age'] = 9;
-
-var n = 'rex';
-var a = 11;
-var d4 = {name:n, age:a};
-
-var name = 'name';
-var age = 'age';
-var d5 = {};
-d5[name] = n;
-d5[age] = a;
-
-dogs.push(d1,d2,d3,d4,d5);
-
-console.log('Dogs:', dogs);
-console.log(d1 instanceof Object);
-console.log(d1 instanceof Dog);
-
-// creating objects using a constructor
-//
-
-function Dog(name, age){
-  this.name = name;
-  this.age = age;
-}
-
-var d6 = new Dog('fluffy', 2);
-
-console.log(d6);
-console.log(d6 instanceof Object);
-console.log(d6 instanceof Dog);
-
-var prompt = require('sync-prompt').prompt;
-
-var option = prompt('(d)og or (q)uit: ');
-while(option !== 'q'){
-  name = prompt('Name: ');
-  age = prompt('Age: ');
-  var dog = new Dog(name,age);
-  dogs.push(dog);
-  option = prompt('(d)og or (q)uit: ');
-}
-
-console.log('All Dogs:', dogs);
+console.log('the area of room 1 is', r1.area());
+console.log('the cost of room 1 is', r1.cost());
+console.log('the cost of room 2 is', r2.cost());
+console.log('the cost of room 3 is', r3.cost());
+console.log('the total area of h1 is', h1.area());
+console.log('the total cost of h1 is', h1.cost());
 
